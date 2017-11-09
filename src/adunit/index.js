605d94ff74a28784d67c961f1fc4620cd39271f6
Valentin Saulyak <valentin.saulyak@gmail.com>
2017-11-09 16:34:45 +0200

    different types of banner added

---
+++ src/adunit/index.js
@@ -0,0 +1,6 @@
+import AdUnit, { adUnitTypes } from './AdUnit';
+import BannerAdUnit from './BannerAdUnit';
+import InterstitialAdUnit from './InterstitialAdUnit';
+
+export default AdUnit;
+export { adUnitTypes, BannerAdUnit, InterstitialAdUnit };
