2b2dce10a5431ddbc0c6cf63b7be89c07592c5cf
Valentin Saulyak <valentin.saulyak@gmail.com>
2017-12-08 14:34:45 +0200

    AdUnit and Adapter classes export from library

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Prebid from './Prebid';
-import { PrebidServerAdapter } from './adapter/index';
-import { BannerAdUnit, InterstitialAdUnit } from './adunit/index';
+import Adapter, { PrebidServerAdapter } from './adapter/index';
+import AdUnit, { BannerAdUnit, InterstitialAdUnit } from './adunit/index';
 import { strategies } from './Settings';
 
 export default Prebid;
 export {
+  Adapter,
   PrebidServerAdapter,
+  AdUnit,
   BannerAdUnit,
   InterstitialAdUnit,
   strategies,
